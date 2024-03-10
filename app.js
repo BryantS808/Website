c3fdba4be645471d7c8f0ac6bda679f912da5eae
BryantS808 <109177265+BryantS808@users.noreply.github.com>
2024-03-10 14:59:23 -0700

    Trying to add vercel capabilties

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+module.exports = app
+//const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Serve static files from the 'public' directory
